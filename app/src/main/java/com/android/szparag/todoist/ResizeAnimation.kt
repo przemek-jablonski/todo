301909336321655259115e10f9cc714ea6ace5a0
sharaquss <sharaquss@gmail.com>
2017-09-16 03:10:01 +0200

    Expandable item in recyclerview

---
+++ app/src/main/java/com/android/szparag/todoist/ResizeAnimation.kt
@@ -0,0 +1,26 @@
+package com.android.szparag.todoist
+
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Transformation
+
+/**
+ * Created by Przemyslaw Jablonski (github.com/sharaquss, pszemek.me) on 9/16/2017.
+ */
+class ResizeAnimation(
+    private val targetView: View, private val targetWidth: Int, private val targetHeight: Int)
+  : Animation() {
+
+  private val startWidth: Int = targetView.width
+  private val startHeight: Int = targetView.height
+
+  override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+    super.applyTransformation(interpolatedTime, t)
+    val newHeight = startHeight + (targetHeight - startHeight) * interpolatedTime
+    targetView.layoutParams.height = newHeight.toInt()
+    targetView.requestLayout()
+  }
+
+  override fun willChangeBounds(): Boolean = true
+
+}
--- app/src/main/java/com/android/szparag/todoist/TodoistWeekActivity.kt
+++ app/src/main/java/com/android/szparag/todoist/TodoistWeekActivity.kt
@@ -1,17 +1,28 @@
 package com.android.szparag.todoist
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import com.android.szparag.todoist.ItemClickSupport.OnItemClickListener
+import kotterknife.bindView
 
 class TodoistWeekActivity : AppCompatActivity() {
+
+  lateinit var displayMetrics : DisplayMetrics
+  val calendarWeekRecyclerView: RecyclerView by bindView(R.id.recyclerview_calendar_week)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -25,11 +36,31 @@ class TodoistWeekActivity : AppCompatActivity() {
           .setAction("Action", null).show()
     }
 
-    (findViewById(R.id.recyclerview_calendar_week) as RecyclerView).apply {
+
+    displayMetrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+
+
+    calendarWeekRecyclerView.apply {
       this.adapter = WeekRvAdapter()
       val layoutManager = LinearLayoutManager(this@TodoistWeekActivity)
       this.layoutManager = layoutManager
-      this.addItemDecoration(android.support.v7.widget.DividerItemDecoration(this.context, layoutManager.orientation) )
+      this.addItemDecoration(DividerItemDecoration(this.context, layoutManager.orientation))
+    }
+
+    ItemClickSupport.addTo(calendarWeekRecyclerView).setOnItemClickListener (object: OnItemClickListener {
+      override fun onItemClicked(recyclerView: RecyclerView, position: Int, view: View) {
+        handleWeekItemClicked(recyclerView, position, view)
+      }
+    })
+  }
+
+  private fun handleWeekItemClicked(recyclerView: RecyclerView, position: Int, view: View) {
+    Snackbar.make(view, "item clicked: $position", Snackbar.LENGTH_LONG).show()
+    ResizeAnimation(view, displayMetrics.widthPixels, displayMetrics.heightPixels).apply {
+      this.duration = 500
+      view.startAnimation(this)
     }
   }
 
